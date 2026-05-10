5213cfe18e89474369c1c99772f4c18c5444b888
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2026-05-10 12:13:12 +0100

    Add files via upload

---
+++ state5.py
@@ -0,0 +1,15 @@
+import time
+
+def state5_change(state5):
+    state5=state5+1
+    if state5 >4:
+        state5=0
+    return state5     
+
+
+print("\033c\033[40;37m\nquantum emulator of a state5\n")
+Qbit=0
+for a in range (20):
+     print(Qbit)
+     Qbit=state5_change(Qbit)
+     
