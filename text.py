de47f85dc0cda9d139cf7fc150719106383d5838
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2026-05-19 19:16:56 +0100

    Add files via upload

---
+++ text.py
@@ -0,0 +1,22 @@
+import tkinter as tk
+
+
+class myapp:
+    def __init__(self,root:tk.Tk):
+        self.root=root
+        self.root.title("text")
+        self.root.configure(background="black")
+        self.txt= tk.Text(background="black",foreground="white")
+        self.txt.pack(padx=10,pady=10)
+        self.bt=tk.Button(background="black",foreground="white",text="press me",command=self.hello)
+        self.bt.pack(padx=10,pady=10)
+        
+    def hello(self):
+       
+        print (self.txt.get("1.00","end"))
+
+
+
+root=tk.Tk()
+apps=myapp(root)
+root.mainloop()
